f36cd43b8a4d9bd4e3a4597d67cc20820e2fd09e
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-02-15 23:21:50 -0500

    feat: better display of unsupported token error

--- mrp/src/error.rs
+++ mrp/src/error.rs
@@ -21,7 +21,7 @@ impl Display for TokenKind {
 
         return match self {
             Literal => write!(f, "literal"),
-            Type => write!(f, "type keyword - 'int' or 'dig'"),
+            Type => write!(f, "type keyword"),
             Ident => write!(f, "identifier"),
             Arrow => write!(f, "pattern seperator"),
             End => write!(f, "\0"),
@@ -36,31 +36,49 @@ pub struct ParseError<'t> {
     pub(crate) kind: ParseErrorKind<'t>,
 }
 
+impl<'t> ParseError<'t> {
+    fn error_location(&self) -> &usize {
+        match &self.kind {
+            ParseErrorKind::ExpectedToken { position, .. } => &position,
+            ParseErrorKind::UnsupportedToken(t) => &t.start,
+        }
+    }
+}
+
 impl<'t> std::error::Error for ParseError<'t> {}
 
 impl<'t> std::fmt::Display for ParseError<'t> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use ParseErrorKind::*;
 
+        writeln!(f, "{}", self.input)?;
+
+        let location = self.error_location();
+
+        for _ in 0..*location {
+            write!(f, " ")?;
+        }
+
+        write!(f, "\u{21B3} @col:{location} ")?;
+
         match &self.kind {
             ExpectedToken {
                 expected,
                 found,
                 text,
-                position,
+                ..
             } => {
-                writeln!(f, "{}", self.input)?;
+                write!(f, "expected a {expected}, but found a {found}, \"{text}\"")
+            }
+            UnsupportedToken(t) => {
+                let result = write!(f, "unsupported token: {} '{}'", t.kind, t.text);
 
-                for _ in 0..*position {
-                    write!(f, " ")?;
+                if let TokenKind::Type = t.kind {
+                    return write!(f, " - supported types are: int, dig");
                 }
 
-                writeln!(
-                    f,
-                    "\u{21B3} @col:{position} expected a {expected}, but found a {found}, \"{text}\""
-                )
+                result
             }
-            UnsupportedToken(t) => write!(f, "unsupported token: {:?}", t),
         }
     }
 }
